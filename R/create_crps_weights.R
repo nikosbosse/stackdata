ac4e2ddca87f9f87d40147397f8ddd6ef5909fed
nikosbosse <nikosbosse@gmail.com>
2020-04-23 00:41:24 +0200

    delete unnecessary code

--- R/create_crps_weights.R
+++ R/create_crps_weights.R
@@ -13,19 +13,3 @@ data <- load_data(regions, h, split_date)$train_data
 # find stacking weights
 stack_crps(data)
 
-
-
-scores_0_7 <- list(rt = rt_scores, case = case_scores) %>% 
-  purrr::map_dfr( 
-    ~ dplyr::filter(., horizon <= 7) %>% 
-      summarise_scores("timeseries"), .id = "type")
-
-scores_8_plus <- list(rt = rt_scores, case = case_scores) %>% 
-  purrr::map_dfr( 
-    ~ dplyr::filter(., horizon > 7) %>% 
-      summarise_scores("timeseries"), .id = "type")
-
-
-saveRDS(data, "~/Desktop/prepared_data_Austria.rds")
-saveRDS(forecast_rts, "~/Desktop/forecast_rt_Austria.rds")
-saveRDS(rt_timeseries, "~/Desktop/true_rt_Austria.rds")
