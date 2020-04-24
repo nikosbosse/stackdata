3fc27197494dc8194085fb919d64601d25be71af
Ben <bbbales2@gmail.com>
2020-04-24 18:48:53 -0400

    Added some arguments to stack_data and a plot

--- R/create_crps_weights.R
+++ R/create_crps_weights.R
@@ -3,13 +3,29 @@ library(tidyverse)
 source("R/functions.R")
 
 #### define parameters to look at
-regions <- list.files("data/")[6]
-h <- 7 # horizon
+regions <- list.files("data/")[2]
+h <- 2 # horizon
 split_date <- as.Date("2020-04-08") # split date into test and training date
 
 # load data
 data <- load_data(regions, h, split_date)$train_data
 
 # find stacking weights
-stack_crps(data)
+R = data %>% pull(geography) %>% unique %>% length
+K = data %>% pull(model) %>% unique %>% length
+S = data %>% pull(sample_nr) %>% unique %>% length
+T = data %>% pull(date) %>% unique %>% length
+stack_crps(data, R = R, K = K, S = S, T = T)
 
+data %>%
+  group_by(model, date) %>%
+  summarize(median = median(y_pred),
+            y_obs = median(y_obs),
+            ql = quantile(y_pred, 0.1),
+            qh = quantile(y_pred, 0.9)) %>%
+  ggplot() +
+    geom_ribbon(aes(date, ymin = ql, ymax = qh), alpha = 0.25) +
+    geom_point(aes(date, median)) +
+    geom_point(aes(date, y_obs), color = "red") +
+    facet_wrap(~ model)
+  
